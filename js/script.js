ef17f9f4ce7a1dfaa210baf66b84078bbf7feac3
emanuelefarelli <emaaliens888@gmail.com>
2023-05-16 16:03:46 +0200

    Added second bonus

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@ for(let i = 0; i < 101; i++){
     if((i % 5 == 0) && (i % 3 == 0)){
         console.log('FizzBuzz');
         p.append(i + ': ' + "FizzBuzz");
+        p.style.color = "violet";
         document.getElementById('my-container').append(p);
     }else if(i % 5 == 0){
         console.log('Buzz');
         p.append(i + ': ' + "Buzz");
+        p.style.color = "yellow";
         document.getElementById('my-container').append(p);
     }else if(i % 3 == 0){
         console.log('Fizz');
         p.append(i + ': ' + "Fizz");
+        p.style.color = "red";
         document.getElementById('my-container').append(p);
     }else{
         console.log(i);
