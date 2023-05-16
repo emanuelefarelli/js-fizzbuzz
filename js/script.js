f59e90313891c6d15d64c022d5f4ea6711013837
emanuelefarelli <emaaliens888@gmail.com>
2023-05-16 15:59:07 +0200

    Added first bonus

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
+
+
 for(let i = 0; i < 101; i++){
+    let p = document.createElement('p');
     if((i % 5 == 0) && (i % 3 == 0)){
         console.log('FizzBuzz');
+        p.append(i + ': ' + "FizzBuzz");
+        document.getElementById('my-container').append(p);
     }else if(i % 5 == 0){
         console.log('Buzz');
+        p.append(i + ': ' + "Buzz");
+        document.getElementById('my-container').append(p);
     }else if(i % 3 == 0){
         console.log('Fizz');
+        p.append(i + ': ' + "Fizz");
+        document.getElementById('my-container').append(p);
     }else{
         console.log(i);
+        p.append(i);
+        document.getElementById('my-container').append(p);
     }
 }
